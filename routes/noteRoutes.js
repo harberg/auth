5ca026e67f08e88521cf7404021766edf72f4340
Nick Harberg <nick@seamonsterstudios.com>
2014-06-09 14:31:07 -0700

    refactored routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,61 +1,63 @@
 'use strict';
 var Note = require('../models/Note');
 
-exports.collection = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  Note.find({}, function(err, notes) {
-    if(err) {
-      res.send(500, {error: err});
-      return false;
-    }
-    res.send(notes);
-  });
-};
+module.exports = function(app, passport) {
+    app.get('/api/v1/notes', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        Note.find({}, function(err, notes) {
+            if(err) {
+                res.send(500, {error: err});
+                return false;
+            }
+            res.send(notes);
+        });
+    });// end app.get collection
 
-exports.findById = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  Note.findOne({'_id': req.params.id}, function(err, note) {
-    if(err) {
-      res.send(500, {error: err});
-      return false;
-    }
-    res.send(note);
-  });
-};
+    app.get('/api/v1/notes/:id', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        Note.findOne({'_id': req.params.id}, function(err, note) {
+            if(err) {
+                res.send(500, {error: err});
+                return false;
+            }
+            res.send(note);
+        });
+    });// end app.get note by id
 
-exports.create = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  var note = new Note({noteBody: req.body.noteBody});
-  note.save(function(err, resNote) {
-    if(err) {
-      res.send(500, {error: err});
-      return false;
-    }
-    res.send(resNote);
-  });
-};
+    app.post('/api/v1/notes', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        var note = new Note({noteBody: req.body.noteBody});
+        note.save(function(err, resNote) {
+            if(err) {
+                res.send(500, {error: err});
+                return false;
+            }
+            res.send(resNote);
+        });
+    });// end app.post
 
-exports.update = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  var id = req.params.id;
-  delete req.body._id;
+    app.put('/api/v1/notes/:id', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        var id = req.params.id;
+        delete req.body._id;
 
-  Note.findOneAndUpdate({'_id' : id}, req.body, function(err, note) {
-    if (err) {
-      res.send(500, {error: err});
-    } else {
-      res.send(note);
-    }
-  });
-};
+        Note.findOneAndUpdate({'_id' : id}, req.body, function(err, note) {
+            if (err) {
+                res.send(500, {error: err});
+            } else {
+                res.send(note);
+            }
+        });
+    });// end app.put
 
-exports.destroy = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  Note.remove({'_id' : req.params.id}, function(err) {
-    if(err) {
-      res.send(500, {error: err});
-      return false;
-    }
-    res.send({'message' : 'success!'});
-  });
-};
+    app.delete('/api/v1/notes/:id', function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        Note.remove({'_id' : req.params.id}, function(err) {
+            if(err) {
+                res.send(500, {error: err});
+                return false;
+            }
+            res.send({'message' : 'success!'});
+        });
+    });// end app.delete
+};// end module.exports
--- server.js
+++ server.js
@@ -4,18 +4,20 @@ var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
-var noteRoutes = require('./routes/noteRoutes');
+//var noteRoutes = require('./routes/noteRoutes');
 
 var app = express();
 app.use(bodyparser.json());
 app.use(express.static( __dirname + '/dist'));
 app.set('port', process.env.PORT || 3000);
 
-app.get('/api/v1/notes', noteRoutes.collection);
-app.post('/api/v1/notes',  noteRoutes.create);
-app.get('/api/v1/notes/:id', noteRoutes.findById);
-app.put('/api/v1/notes/:id', noteRoutes.update);
-app.delete('/api/v1/notes/:id', noteRoutes.destroy);
+require('./routes/noteRoutes')(app, passport);
+
+// app.get('/api/v1/notes', noteRoutes.collection);
+// app.post('/api/v1/notes',  noteRoutes.create);
+// app.get('/api/v1/notes/:id', noteRoutes.findById);
+// app.put('/api/v1/notes/:id', noteRoutes.update);
+// app.delete('/api/v1/notes/:id', noteRoutes.destroy);
 mongoose.connect('mongodb://localhost/notes-development');
 
 var server = http.createServer(app);
